7bb4ee7e645c23d3e9dc94c8a2f9c7faf5348413
esteban <estebanfernandez20003024@gmail.com>
2022-08-08 17:28:18 -0300

    sda

--- secciones/socios.js
+++ secciones/socios.js
@@ -4,21 +4,33 @@ const formSesion = document.getElementById("form-sesion")
 const logOut = document.getElementById("log-out")
 const mainvideo = document.getElementById("videonone")
 const haceteSocios = document.getElementById("haceteSocio")
+const contrasenias= document.getElementById("contraseniaR")
+const usuario = document.getElementById("usuarioR")
+const formRegistro = document.getElementById("registro")
+const botonR = document.getElementById("botonR")
+const registrarse = document.getElementById("registrarse")
 
 
 
-const user = {
-    usuario: "esteban3024",
-    contrasenia: "1234567"
+formRegistro.onsubmit = (e) =>{
+     e.preventDefault()
+     var nombre = usuario.value;
+     var contrasenia = contrasenias.value;
+   localStorage.setItem("usuarios", nombre)
+   localStorage.setItem("contraseniass", contrasenia)
+   
 }
 
+registrarse.onclick = () => {
+    registro.style.display = "block"
 
-
+    formSesion.style.display = "none"
+}
 
 
 formSesion.onsubmit = (e) => {
     e.preventDefault()
-    if ( inputUsuario.value === user.usuario && inputContrasenia.value === user.contrasenia ) {
+    if ( inputUsuario.value === usuarios.value && inputContrasenia.value === contraseniass.value ) {
         mainvideo.style.display = "flex"
         formSesion.style.display = "none"
      
