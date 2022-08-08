df065367896a9ba67d3c209d4fc4ad5a73efc9db
esteban <estebanfernandez20003024@gmail.com>
2022-08-08 19:59:16 -0300

    sasa

--- secciones/socios.js
+++ secciones/socios.js
@@ -2,24 +2,63 @@ const inputUsuario = document.getElementById("usuario")
 const inputContrasenia = document.getElementById("contrasenia")
 const formSesion = document.getElementById("form-sesion")
 const logOut = document.getElementById("log-out")
-const mainvideo = document.getElementById("videonone")
+const mainsocios = document.getElementById("mainsocios")
 const haceteSocios = document.getElementById("haceteSocio")
-const contrasenias= document.getElementById("contraseniaR")
-const usuario = document.getElementById("usuarioR")
+const contraseniaR= document.getElementById("contraseniaR")
+const usuarioR = document.getElementById("usuarioR")
 const formRegistro = document.getElementById("registro")
 const botonR = document.getElementById("botonR")
 const registrarse = document.getElementById("registrarse")
+const beneficioss = document.getElementById("beneficios")
 
 
+let  nuevoshinchas = []
+let usuariR = []
+let contraR = []
 
-formRegistro.onsubmit = (e) =>{
-     e.preventDefault()
-     var nombre = usuario.value;
-     var contrasenia = contrasenias.value;
-   localStorage.setItem("usuarios", nombre)
-   localStorage.setItem("contraseniass", contrasenia)
-   
+const pushearUsuario = (array, valor) => {
+array.push(valor)
 }
+const subirls = (clave, valor) => {
+    const arrayjson = JSON.stringify(valor)
+    localStorage.setItem(clave, arrayjson)
+}
+
+
+
+formRegistro.onsubmit = (e) => {
+e.preventDefault()
+pushearUsuario(usuariR, usuarioR.value);
+pushearUsuario(contraR, contraseniaR.value)
+subirls("usuarios", usuariR)
+subirls("contrasenia", contraR)
+formSesion.style.display = "block"
+formRegistro.style.display = "none"
+}
+const traerdels = (clave) => {
+    const arraydels = localStorage.getItem(clave)
+    const parseararray = JSON.parse(arraydels)
+    return parseararray
+    
+    }
+    const traerdelss = (clave) => {
+        const arraydelss = localStorage.getItem(clave)
+        const parseararrayy = JSON.parse(arraydelss)
+        return parseararrayy
+        
+        }
+
+
+
+
+
+let usuarioLS = traerdels("usuarios")
+console.log(usuarioLS)
+
+let contraseniaLS = traerdelss("contrasenia")
+console.log(contraseniaLS)
+
+
 
 registrarse.onclick = () => {
     registro.style.display = "block"
@@ -30,10 +69,11 @@ registrarse.onclick = () => {
 
 formSesion.onsubmit = (e) => {
     e.preventDefault()
-    if ( inputUsuario.value === usuarios.value && inputContrasenia.value === contraseniass.value ) {
-        mainvideo.style.display = "flex"
+ 
+    if (Number(inputContrasenia.value) === Number(contraseniaLS)){
+        mainsocios.style.display = "flex"
         formSesion.style.display = "none"
-     
+        beneficios.style.display = "none"
         localStorage.setItem("user", true)
     } else {
         formSesion.reset()
@@ -44,8 +84,9 @@ formSesion.onsubmit = (e) => {
 function preferenciaDeUsuario () {
     const tokenLS = localStorage.getItem("user")
     if ( tokenLS === "true" ) {
-        mainvideo.style.display = "flex"
-
+        mainsocios.style.display = "flex"
+       
+        logOut.style.display = "flex"
         formSesion.style.display = "none"
      
     } else if ( tokenLS !== "true" ) {
